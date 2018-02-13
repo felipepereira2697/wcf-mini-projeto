5b59e5afd8f7782fe8bb1cf3ae5163a7de688bed
Felipe Pereira <felipepereira2697@gmail.com>
2018-02-12 17:03:15 -0800

    Adição das anotações no contrato e no que vamos serializar

--- Jogadores/IJogadorService.cs
+++ Jogadores/IJogadorService.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Jogadores
 {
+    //Para sinalizar ao wcf que esse é o nosso contrato
+    [ServiceContract]
     public interface IJogadorService
     {
+        [OperationContract]
         void Adicionar(Jogador j);
+        [OperationContract]
         Jogador Buscar(string nome);
 
     }
--- Jogadores/Jogador.cs
+++ Jogadores/Jogador.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Jogadores
 {
+    [DataContract]
     public class Jogador
     {
+        [DataMember]
         public string Nome  { get; set; }
+        [DataMember]
         public string Time { get; set; }
         
 
